ebc7f6d5203b183850ac659145c868b2e66485bc
ostrea <9917165+ostrea@users.noreply.github.com>
2021-09-10 17:17:24 +0300

    Add a main logic

---
+++ app/app.go
@@ -0,0 +1,59 @@
+package app
+
+import (
+	"github.com/ostrea/wanikanileechsuspender/pkg/database"
+	"github.com/ostrea/wanikanileechsuspender/pkg/wkapi"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"math"
+	"os"
+)
+
+func Run() {
+	log.Println("Leach suspension started.")
+	var leechIds []int
+
+	allSubjectStats := wkapi.GetAllSubjectStats()
+	burnedSubjectIds := wkapi.GetBurnedSubjectIds()
+	for _, subject := range allSubjectStats {
+		if subject.SubjectType != "vocabulary" {
+			continue
+		}
+
+		// Some kanji moved beyond current level, and have 0 in streak.
+		// They were unlocked ages ago, so they have an entry in stats.
+		if subject.MeaningCurrentStreak == 0 {
+			continue
+		}
+
+		if _, ok := burnedSubjectIds[subject.SubjectId]; ok {
+			continue
+		}
+
+		leechScoreMeaning := float64(subject.MeaningIncorrect) / (1.5 * float64(subject.MeaningCurrentStreak))
+		var leechScoreReading float64
+		if subject.SubjectType != "radical" {
+			leechScoreReading = float64(subject.ReadingIncorrect) / (1.5 * float64(subject.ReadingCurrentStreak))
+		}
+
+		leechScore := math.Max(leechScoreMeaning, leechScoreReading)
+		if leechScore >= 2 {
+			leechIds = append(leechIds, subject.SubjectId)
+		}
+	}
+
+	leechSubjects := wkapi.GetSpecifiedSubjects(leechIds)
+	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.AutoMigrate(&database.Leech{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	database.SaveLeeches(db, leechSubjects)
+	log.Println("Leach suspension ended.")
+}
